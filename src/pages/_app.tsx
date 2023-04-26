9f8967659ee225c3d860897fc388df50384a4d6a
ISNewton <ashrfalhag.11@gmail.com>
2023-04-26 13:19:17 +0200

    add custom layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,19 +6,30 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import Layout from "~/layouts/Layout";
+import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from "react";
+import { NextPage } from "next";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout || ((page) => <Layout />)
+
   return (
-    <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      {getLayout(<Component {...pageProps} />)}
 
-      </Layout>
     </SessionProvider>
-  );
-};
+  )
+
+}
+
+
 
 export default api.withTRPC(MyApp);
