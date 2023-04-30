0f7ed903a931e483416221caddfbd4df2323a544
ISNewton <ashrfalhag.11@gmail.com>
2023-04-30 19:14:37 +0200

    separate search bar

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,26 +1,19 @@
 import { signOut, useSession } from "next-auth/react"
 import Button from "~/components/buttons/Button"
+import UsersSearch from "~/components/forms/UsersSearch"
 
 export default () => {
-    const session = useSession()    
-    
+    const session = useSession()
+
 
     return (
         <div className="px-5 py-5 flex justify-between items-center bg-white border-b-2">
             <div className="font-semibold text-2xl">GoingChat , {session.data?.user.username}</div>
-        
-            <div className="w-1/2">
-                <input
-                    type="text"
-                    name=""
-                    id=""
-                    placeholder="search IRL"
-                    className="rounded-2xl bg-gray-100 py-3 px-5 w-full"
-                />
-            </div>
-                <Button onClick={() => signOut()}>
-                    Log out
-                </Button>
+
+            <UsersSearch />
+            <Button onClick={() => signOut()}>
+                Log out
+            </Button>
             <div
                 className="h-12 w-12 p-2 bg-yellow-500 rounded-full text-white font-semibold flex items-center justify-center"
             >
